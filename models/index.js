e8e44a3c04a567fcba31c5ab8b0ffd73f095914e
Patrick Nascimento <patrickndeoliveira@gmail.com>
2021-07-12 11:12:48 -0300

    configuração do sequelize adaptada às configurações das variáveis de ambiente

--- models/index.js
+++ models/index.js
@@ -1,11 +1,23 @@
 'use strict';
 
+require("dotenv").config({
+  allowEmptyValues: true
+});
+
+if (process.env.NODE_ENV && process.env.NODE_ENV === 'test'){
+  require("dotenv").config({
+    allowEmptyValues: true,
+    path: '.env'
+  });
+}
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV.trim();
+
+const config = require('../config/config');
 const db = {};
 
 let sequelize;
