5e634ee51b9e12ee803e1f6470e805da3f527a51
Patrick Nascimento <patrickndeoliveira@gmail.com>
2021-07-12 21:06:30 -0300

    feat(session.test.js):Criado um teste para garantir que a API está retornando o status 401 quando o usuário enviar uma senha incorreta

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -34,6 +34,26 @@ describe('Authentication', () => {
         expect(response.status).toBe(200)
     });
 
+    //Teste para garantir que a rota de autenticação está recusando usuários incorretos
+    it('should auth with valid credentials', async () => {
+        //Cria um usuário no banco de dados sqlite
+        const user = await users.create({
+            firstName: 'Patrick',
+            lastName: 'Nascimento',
+            email: 'patrickndeoliveira@gmail.com',
+            password: await bcrypt.hash('123456', 12)
+        })
+        //Faz uma requisição para a rota de autenticação com credenciais inválidas
+        const response = await request(app)
+            .post('/sessions')
+            .send({
+                email: user.email,
+                password: 'senhaErrada'
+            })
+        //Espera que o status retornado seja 401 (unauthorized)
+        expect(response.status).toBe(401)
+    });
+
     //Teste para garantir que a rota de autenticação está retornando um jwt token
     it('should return jwt token when authenticated', async () => {
         //Cria um usuário no banco de dados sqlite
