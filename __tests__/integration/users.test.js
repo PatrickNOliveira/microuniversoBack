71033d786b9417b079b38cf1df02e5b97b2e2b10
Patrick Nascimento <patrickndeoliveira@gmail.com>
2021-07-12 21:49:00 -0300

    feat(users.test.js): Criado o teste de integração para a criação de novos usuários

---
+++ __tests__/integration/users.test.js
@@ -0,0 +1,35 @@
+const request = require('supertest')
+const bcrypt = require('bcrypt')
+const app = require('../../index')
+const truncate = require('../../utils/truncate')
+const factory = require('../factories')
+const faker = require('faker')
+
+
+describe('Users', () => {
+    //Antes de cada teste, elimina os dados do banco sqlite
+    beforeEach(async () => {
+        await truncate()
+    })
+    //Após cada teste, finaliza o servidor node
+    afterEach(() => {
+        app.close()
+    })
+
+    //Teste para garantir que o usuário está sendo criado na rota post (/register)
+    it('should create user with valid data', async () => {
+        //Faz uma requisição para a rota de registro com os dados a serem inseridos no sistema
+        const response = await request(app)
+            .post('/register')
+            .send({
+                //Usar o módulo faker para gerar dados aleatórios para o test
+                firstName: faker.name.firstName(),
+                lastName: faker.name.lastName(),
+                email: faker.internet.email(),
+                password: bcrypt.hash(faker.internet.password(), 12)
+            })
+
+        //Espera-se que o status retornado seja 201 (Created)
+        expect(response.status).toBe(201)
+    });
+})
