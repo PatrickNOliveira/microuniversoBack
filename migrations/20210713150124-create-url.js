368597965b84ca59638eed6981279b1430616f20
Patrick Nascimento <patrickndeoliveira@gmail.com>
2021-07-13 12:44:13 -0300

    feat(url.js/create-url.js): Model de url curta criado, migration também criada

---
+++ migrations/20210713150124-create-url.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Urls', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      destiny: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      tinyUrl: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Urls');
+  }
+};
---
+++ models/url.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Url extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Url.init({
+    destiny: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull:{
+          msg: "Campo destiny é obrigatório !"
+        }
+      }
+    },
+    tinyUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull:{
+          msg: "Campo tinyUrl é obrigatório !"
+        }
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull:{
+          msg: "Campo user_id é obrigatório !"
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Url',
+  });
+  return Url;
+};
