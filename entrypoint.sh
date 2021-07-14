87a35edb1513715fe8ab58525903ae16c4348364
Patrick Nascimento <patrickndeoliveira@gmail.com>
2021-07-14 13:57:07 -0300

    build(entrypoint.sh/Dockerfile/docker-compose): Banco de dados containizado, criei um shellscript para a aplicação rodar as migrations após o início do banco de dados sempre que a aplicação subir

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+##Verifica de o mysql já iniciou
+until mysqlshow --user=root --password=microuniverso -h database-micro -P 3306 --protocol=tcp > /dev/null 2>&1; do
+        echo "Aguardando mysql";
+        sleep 5;
+done
+  ##Se iniciou, roda as migrations e inicia a aplicação
+  npx sequelize-cli db:migrate
+  npm start
