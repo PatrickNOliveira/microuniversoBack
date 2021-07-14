95ab3cd8d811c64ac415b7efb8c9db3a2c0b469c
Patrick Nascimento <patrickndeoliveira@gmail.com>
2021-07-14 00:25:51 -0300

    feat (url.test.js): Teste de integração para garantir que o usuário logado salve seu id na URL ao criá-la

--- utils/genericUrlGenerate.js
+++ utils/genericUrlGenerate.js
@@ -3,8 +3,7 @@ const faker = require('faker')
 async function generateUrl(){
     return {
         destiny: faker.internet.url(),
-        tinyUrl: faker.lorem.word(),
-        user_id: Math.floor(faker.datatype.number())
+        tinyUrl: faker.lorem.word()
     }
 
 }
