987d222529e95072977774f26ac1e705d91a2f39
Patrick Nascimento <patrickndeoliveira@gmail.com>
2021-07-15 00:04:25 -0300

    feat(UrlService/urlController): Criada função para buscar URL por codigo

--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -4,5 +4,6 @@ const optionalAuth = require('../middleware/optionalAuth')
 
 const router = Router()
 router.post('/url', optionalAuth, urlController.criarUrl)
+router.get('/url/:codigo', urlController.buscarPeloCodigo)
 
 module.exports = router
--- services/UrlService.js
+++ services/UrlService.js
@@ -1,7 +1,14 @@
 const Services = require('./Services')
+const database = require('../models')
 class UrlService extends Services {
     constructor() {
         super('Url');
+    }
+
+    async searchForCode(code){
+        return await database[this.model].findOne({
+            where:{tinyUrl: code}
+        })
     }
 }
 
