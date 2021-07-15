539e0b1a1a19dc31f960400f1fceb04f8bfe7097
Patrick Nascimento <patrickndeoliveira@gmail.com>
2021-07-15 02:25:03 -0300

    fix(urlController.js): Readicionado o bloco de código para autenticação opcional

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -7,6 +7,9 @@ class UrlController {
         try {
 
             const url = req.body
+            if (req.user) {
+                url.user_id = req.user.id
+            }
             const urlCriada = await urlService.insertData(url)
             return res.status(201).json(urlCriada)
 
