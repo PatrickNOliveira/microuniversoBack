aa2931b363b6e9006df593862478f53b19cb1b10
Patrick Nascimento <patrickndeoliveira@gmail.com>
2021-07-14 23:54:21 -0300

    feat(url.test.js): Teste para garantir que a rota de busca de URL está retornando a URL correta criado

--- __tests__/integration/url.test.js
+++ __tests__/integration/url.test.js
@@ -104,4 +104,17 @@ describe('Url', ()=>{
        //Espera-se que o status retornado seja 200 (Ok)
        expect(response.status).toBe(200)
    });
+
+    /************** Teste para garantir que a rota de busca de URL está retornando a URL correta ***************/
+    it('should be return a status 200', async () => {
+        //Gera dados genéricos para a URL
+        const url = factory.create('Url')
+
+        //Faz uma requisição para a rota de busca usando o código da URL
+        const response = await request(app)
+            .get('/url/'+url.tinyUrl)
+
+        //Espera-se que o status retornado seja 200 (Ok)
+        expect(response.body).toBe(200)
+    });
 })
