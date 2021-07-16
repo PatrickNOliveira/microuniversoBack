e6c14e2e2f6ea731b3175106c8fb94627ae34cc4
Patrick Nascimento <patrickndeoliveira@gmail.com>
2021-07-16 09:22:12 -0300

    Última alteração no entrypoint.sh para mandar o app para rpod

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 ##Verifica de o mysql já iniciou
-while ! curl -o - database-micro:3306;
-do
-  echo "Aguardando mysql"
+while ! curl -o - database-micro:3306; do
   sleep 5;
 done
-  ##Se iniciou, roda as migrations e inicia a aplicação
-npx sequelize-cli db:migrate
-npm start
+  npx sequelize db:migrate
+  npm start
