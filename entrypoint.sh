1c78d2d70f72f99913c8342d8dbe857ad58dd1c7
Patrick Nascimento <patrickndeoliveira@gmail.com>
2021-07-16 09:32:59 -0300

    Adicionados comentários ao entrypoint para facilitar o entendimento

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 ##Verifica de o mysql já iniciou
 while ! curl -o - database-micro:3306; do
+  ##Repete de 5 em 5 segundos até que obtenha uma resposta
   sleep 5;
 done
+  #Quando obtiver a resposta, roda as migrations e inicia o sistema
   npx sequelize db:migrate
   npm start
